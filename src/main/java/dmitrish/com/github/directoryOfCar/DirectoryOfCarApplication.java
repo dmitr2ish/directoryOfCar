efe7d8bde0a4be01dab962eeecd7d088d4a31d3a
dmitr2ish <dmitr2ish@yandex.ru>
2020-08-10 09:42:58 +0500

    - init

---
+++ src/main/java/dmitrish/com/github/directoryOfCar/DirectoryOfCarApplication.java
@@ -0,0 +1,13 @@
+package dmitrish.com.github.directoryOfCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoryOfCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectoryOfCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmitrish/com/github/directoryOfCar/DirectoryOfCarApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitrish.com.github.directoryOfCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectoryOfCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
